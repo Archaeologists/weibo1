3803d0c544d2f6a8f64a7284a02d85ce5152086b
zhong <1046706092@qq.com>
2023-04-21 09:32:16 +0000

    更改用户资料

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -10,20 +10,20 @@
                     </a>
                     <div class="dropdown-menu" aria-labelledby="navbarDropdown">
                         <a class="dropdown-item" href="{{ route('users.show',Auth::user()) }}">个人中心</a>
-                        <a class="dropdown-item" href="#">编辑资料</a>
+                        <a class="dropdown-item" href="{{ route('users.edit',Auth::user()) }}">编辑资料</a>
                         <div class="dropdown-divider"></div>
                         <a class="dropdown-item" id="logout" href="#">
                             <form action="{{ route('logout') }}" method="POST">
                             {{ csrf_field() }}
                             {{ method_field('DELETE') }}
-                            <button class="btn btn-block btn-danger" type="submit" name="button">退出</button>
+                            <button class="btn btn-block btn-danger" style="width: 100%" type="submit" name="button">退出</button>
                            </form>
                         </a>
                     </div>
                 </li>
             @else
             <li class="nav-item"><a class="nav-link" href="{{ route('help') }}">帮助</a></li>
-            <li class="nav-item"><a class="nav-link" href="#">登录</a></li>
+            <li class="nav-item"><a class="nav-link" href="{{ route('login') }}">登录</a></li>
             @endif
         </ul>
     </div>
--- resources/views/shared/_user_info.blade.php
+++ resources/views/shared/_user_info.blade.php
@@ -1,5 +1,5 @@
 <a href="{{ route('users.show',$user->id) }}">
-  <img src="https://cdn.learnku.com/uploads/sites/KDiyAbV0hj1ytHpRTOlVpucbLebonxeX.png" width="140" height="140" alt="{{ $user->name }}" class="gravatar">
+  <img src="https://cdn.learnku.com/uploads/sites/KDiyAbV0hj1ytHpRTOlVpucbLebonxeX.png" width="140" height="70" alt="{{ $user->name }}" class="gravatar">
 </a>
 
 <h1>{{ $user->name }}</h1>
