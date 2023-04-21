48234470856f0c7d48f87f2703e23465df64c5d0
zhong <1046706092@qq.com>
2023-04-21 12:45:34 +0000

    删除用户

--- resources/views/users/_user.blade.php
+++ resources/views/users/_user.blade.php
@@ -3,4 +3,12 @@
     <a href="{{ route('users.show',$user) }}">
        {{ $user->name }}
     </a>
+    @can('destroy', $user)
+        <form method="POST" action="{{ route('users.destroy',$user->id) }}" class="float-end">
+            {{ csrf_field() }}
+            {{ method_field('DELETE') }}
+
+            <button class="btn btn-sm btn-danger delete-ben" type="submit">删除</button>
+        </form>
+    @endcan
 </div>
