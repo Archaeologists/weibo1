df8dde08684842bf25495365d8b620ba4dcc4cb6
zhong <1046706092@qq.com>
2023-04-25 00:10:22 +0000

    用户微博列表

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,6 +23,8 @@ class DatabaseSeeder extends Seeder
         // ]);
         Model::unguard();
         $this->call(UsersTableSeeder::class);
+        $this->call(StatusesTableSeeder::class);
         Model::reguard();
+
     }
 }
